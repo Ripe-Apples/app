733a0608e8c697993e9c398cc134c2bbb3fbf2e7
Lucas Levin <lucaslevin1@gmail.com>
2018-08-09 17:12:28 -0400

    added score logic

--- client/components/restaurant-card.js
+++ client/components/restaurant-card.js
@@ -8,6 +8,22 @@ const dollarSignHelper = number => {
 
 const RestaurantCard = props => {
   const restaurant = props.restaurant
+  const restaurantScore = Math.round(
+    restaurant.reviews
+      .map(review => {
+        if (review.source === 'Yelp') {
+          return review.rating / 5
+        } else if (review.source === 'Trip Advisor') {
+          return review.rating / 5
+        } else {
+          return review.rating / 5
+        }
+      })
+      .reduce((accum, currentVal) => accum + currentVal, 0) /
+      restaurant.reviews.length *
+      100,
+    0
+  )
 
   return (
     <div className="card">
@@ -18,7 +34,8 @@ const RestaurantCard = props => {
         <div className="header">{restaurant.name}</div>
         <div className="meta">{restaurant.location}</div>
         <div className="description">
-          {dollarSignHelper(restaurant.expenseRating)}, {restaurant.cuisineType}
+          {dollarSignHelper(restaurant.expenseRating)}, {restaurant.cuisineType},{' '}
+          {restaurantScore}
           <span className="right floated">
             <Link to="/sourceComp">View Sources</Link>
           </span>
