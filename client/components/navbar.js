ee7fc6498e97202951dc1e260bca7bf93f6d00ce
Lucas Levin <lucaslevin1@gmail.com>
2018-08-09 14:09:15 -0400

    small header tweaks

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -5,26 +5,28 @@ import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/">Ripe Tomato</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
+  <div className="ui top menu">
+    <div className="item">
+      <a to="/">Ripe Apples</a>
+    </div>
+    {isLoggedIn ? (
+      <div>
+        {/* The navbar will show these links after you log in */}
+        <a href="#" onClick={handleClick} className="item">
+          Logout
+        </a>
+      </div>
+    ) : (
+      <div>
+        {/* The navbar will show these links before you log in */}
+        <a to="/login" className="item">
+          Login
+        </a>
+        <a to="/signup" className="item">
+          Sign Up
+        </a>
+      </div>
+    )}
   </div>
 )
 
