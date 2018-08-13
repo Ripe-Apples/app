ba4b3eb84f1db9109b93f0ddca647c684789e4ca
Jordan Marcus <jormarcus92@gmail.com>
2018-08-13 17:22:52 -0400

    cant get google data

--- script/google.js
+++ script/google.js
@@ -1,11 +1,14 @@
 const axios = require('axios')
+const {kevKey, googleKey, gKey2, key4} = require('../secrets');
 
-async function getGoogleRestaurants (key) {
-  const res = await axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=New+York+food&sensor=false&location=40.67,-73.94&radius=100&key=${key}`
-  )
+
+const apiRoute = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJOwg_06VPwokRYv534QaPC8g&fields=name,rating&types["restaurant"]&key='
+
+async function getGoogleRestaurants (route, key) {
+  const res = await axios.get(route + key)
   console.log(res.data)
 }
 
 
-getGoogleRestaurants('AIzaSyDOj3QxjVpY6V9DaKdcC4LtwPInSB14e2k')
+getGoogleRestaurants(apiRoute, key4)
 
