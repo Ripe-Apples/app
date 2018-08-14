3f1d24c02d1d62c8f3843fb0246077e21b4dd4c8
Jordan Marcus <jormarcus92@gmail.com>
2018-08-14 11:07:31 -0400

    added function to parse google name in query

--- script/google.js
+++ script/google.js
@@ -4,11 +4,15 @@ const {kevKey, googleKey, gKey2, key4} = require('../secrets');
 
 const apiRoute = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJOwg_06VPwokRYv534QaPC8g&fields=name,rating&types["restaurant"]&key='
 
+const apiRoute2 = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+New+York&key='
+
 async function getGoogleRestaurants (route, key) {
   const res = await axios.get(route + key)
   console.log(res.data)
 }
 
 
-getGoogleRestaurants(apiRoute, key4)
+const googleRestaurants = getGoogleRestaurants(apiRoute2, key4)
+
+console.log(googleRestaurants.length)
 
---
+++ script/googleUsingYelp.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const axios = require('axios')
+const {yelpApiKey, googleKey5} = require('../secrets')
+const db = require('../server/db')
+const {Restaurant, Review} = require('../server/db/models')
+
+const iconUrl = 'https://image.flaticon.com/icons/svg/174/174882.svg'
+
+async function getYelp(offset) {
+  const {data} = await axios.get(
+    `https://api.yelp.com/v3/businesses/search?term=restaurants&location=new-york&limit=50&offset=${offset}`,
+    {
+      headers: {
+        Authorization: `Bearer ${yelpApiKey}`
+      }
+    }
+  )
+  return data.businesses
+}
+
+async function getRestaurants() {
+  let restaurants = []
+
+  for (let i = 0; i < 20; i++) {
+    let response = await getYelp(i * 50)
+    restaurants = restaurants.concat(Array.from(response))
+  }
+
+  return restaurants
+}
+
+async function getGooglerestaurants() {
+
+  //get yelp restaurants
+  const restaurants = await getRestaurants();
+
+  //loop through array and convert names of restaurants to be used in google query
+  for (let i = 0; i < restaurants.length; i++) {
+    let restaurant = restaurants[i];
+    let convertedName = restaurant.name.split(' ').join('%20');
+
+    //this gets a single restaurant from google but will be repeated for each restaurant in yelp
+    const res = await axios.get(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${convertedName}&inputtype=textquery&fields=name,rating&key=${googleKey5}`)
+
+    return res.data;
+  }
+
+}
+
+const googleRestaurants = getGooglerestaurants();
+
+
+//put these in the same seed file as yelp and everything else to store the db???
