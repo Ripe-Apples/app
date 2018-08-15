f8c60a1051e58d29a457909fc585b919e0f4adc1
Lucas Levin <lucaslevin1@gmail.com>
2018-08-15 12:39:56 -0400

    pulled google data

--- script/foursquareTestScript.js
+++ script/foursquareTestScript.js
@@ -1,34 +1,59 @@
 const {Restaurant} = require('../server/db/models')
 const axios = require('axios')
 const {foursquareClientId, foursquareApiKey} = require('../secrets')
+const fs = require('fs')
 
-async function fetchRestaurants() {
+// async function get4sq(offset) {
+//   try {
+//     const {data} = await axios.get(
+//       `https://api.foursquare.com/v2/venues/explore?section=food&offset=${offset}&near=New York, NY&client_id=${foursquareClientId}&client_secret=${foursquareApiKey}&v=20180814`
+//     )
+//     return data.response.groups[0].items.map(item => {
+//       const venue = item.venue
+//       return {
+//         id: venue.id,
+//         name: venue.name
+//       }
+//     })
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
+// async function get4sqIds() {
+//   try {
+//     let finalArr = []
+//     for (let i = 0; i <= 240; i += 30) {
+//       let tempArr = await get4sq(i)
+//       finalArr = finalArr.concat(tempArr)
+//     }
+//     fs.writeFile('script/foursquareData.json', JSON.stringify(finalArr))
+//     console.log('Done!')
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
+async function get4sqRatings() {
   try {
-    const restaurants = await Restaurant.findAll()
-    return restaurants
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-async function readRestaurants() {
-  const restaurants = await fetchRestaurants()
-  let finalArray = []
-  restaurants.forEach(restaurant => {
-    finalArray.push(restaurant.dataValues)
-  })
-  console.log(finalArray)
-}
-
-async function get4sq() {
-  try {
-    const {data} = await axios.get(
-      `https://api.foursquare.com/v2/venues/search?near=New York, NY&query=Locanda Verde&client_id=${foursquareClientId}&client_secret=${foursquareApiKey}&v=20180814`
+    const idArr = JSON.parse(
+      fs.readFileSync('script/foursquareData.json', 'utf-8')
     )
-    console.log(data.response.venues[0])
+    let finalArr = []
+    for (let i = 0; i < 10; i++) {
+      const {data} = await axios.get(
+        `https://api.foursquare.com/v2/venues/${
+          idArr[0].id
+        }?client_id=${foursquareClientId}&client_secret=${foursquareApiKey}&v=20180814`
+      )
+      let venue = data.response.venue
+      finalArr.push({name: venue.name, rating: venue.rating})
+    }
+    fs.writeFile('script/finalFoursquareData.json', JSON.stringify(finalArr))
+    console.log('done!')
   } catch (error) {
     console.error(error)
   }
 }
 
-get4sq()
+get4sqRatings()
