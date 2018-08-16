7909564168284c09997ece1471a5f375ed1f4002
Jordan Marcus <jormarcus92@gmail.com>
2018-08-15 21:03:48 -0400

    reset works on filters when you click home

--- client/components/SingleRestaurant.js
+++ client/components/SingleRestaurant.js
@@ -1,70 +1,57 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {fetchRestaurants} from '../store/restaurant'
 import {fetchSingleRestaurant} from '../store/restaurant'
-import RestaurantCard from './restaurant-card'
-import {Input, Grid, Container, Image, Divider, Header, Link, Label} from 'semantic-ui-react'
-//import CircularProgress from '@material-ui/core/CircularProgress'
+import {Grid, Container, Image, Divider, Header, Label} from 'semantic-ui-react'
 
 const dollarSignHelper = expenseRating => {
     if (expenseRating === 0) return 'No Expense Rating Yet'
     return '$'.repeat(expenseRating)
-  }
+}
 
 class SingleRestaurant extends Component {
-    constructor() {
-      super()
-      this.state = {
-        isDisabled: false
-      }
-    }
-  
-    componentDidMount() {
-        console.log('ID!!', this.props.match.params.restaurantId)
-        this.props.fetchSingleRestaurant(this.props.match.params.restaurantId)
-     
-    }
-    render() {
-        const {singleRestaurant} = this.props
-        console.log("rating", singleRestaurant.expenseRating)
+  componentDidMount() {
+    this.props.fetchSingleRestaurant(this.props.match.params.restaurantId)
+  }
 
-        if (!singleRestaurant) {
-            return null;
-        } else {
-            return (
-                <Container>
-                    <Divider hidden/>
-                    <Divider hidden/>
-                    <Divider hidden/>
-                <Grid> 
-                    <Grid.Column width={10}>
-                     <Image src={singleRestaurant.imageUrl} size="big"  rounded />
-                     </Grid.Column>
-                     <Grid.Column width={6}>
-                     <Header size="huge" color="orange"> {singleRestaurant.name} </Header>
-                     <p className={singleRestaurant.expenseRating <= 2 ? "cheap" : "expensive" }>Expense Rating: {dollarSignHelper(singleRestaurant.expenseRating)}</p>
-                     <Header size="medium">Address: {singleRestaurant.location}</Header>
-                     <Divider hidden/>
-                        <Label>
-                            <img className="map-logo" src = {"https://image.flaticon.com/icons/svg/281/281767.svg"} />
-                            <Label.Detail>
-                            <br />
-                                <a href={"https://www.google.com/maps/search/" + singleRestaurant.location}> View Restaurant Location</a>
-                            </Label.Detail>
-                            
-                        </Label>                       
-                     </Grid.Column>
-                     
-                     </Grid>
-                     </Container>
-            )
-        }
+  render() {
+    const { singleRestaurant } = this.props
 
-}
+    if (!singleRestaurant) {
+      return null;
+    } else {
+      return (
+        <Container>
+          <Divider hidden />
+          <Divider hidden />
+          <Divider hidden />
+          <Grid>
+            <Grid.Column width={10}>
+              <Image src={singleRestaurant.imageUrl} size="big" rounded />
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <Header size="huge" color="orange"> {singleRestaurant.name} </Header>
+              <p className={singleRestaurant.expenseRating <= 2 ? "cheap" : "expensive"}>Expense Rating: {dollarSignHelper(singleRestaurant.expenseRating)}</p>
+              <Header size="medium">Address: {singleRestaurant.location}</Header>
+              <Divider hidden />
+              <Label>
+                <img className="map-logo" src={"https://image.flaticon.com/icons/svg/281/281767.svg"} />
+                <Label.Detail>
+                  <br />
+                  <a href={"https://www.google.com/maps/search/" + singleRestaurant.location}> View Restaurant Location</a>
+                </Label.Detail>
+
+              </Label>
+            </Grid.Column>
+
+          </Grid>
+        </Container>
+      )
+    }
+
+  }
 }
 
 const mapState = state => {
-    console.log('state!!', state.restaurantReducer)
     return {
         singleRestaurant: state.restaurantReducer.singleRestaurant
     }
